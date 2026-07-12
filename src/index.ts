e98ba38ca34f350e7665ecc0cdb43ea5cbc4f343
xTymezzAlt <fete3712@gmail.com>
2026-07-12 19:26:19 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
 import { Workspace } from "./Workspace";
 import { Block } from "./Block";
+import { Events } from "./Events";
+import { Connection } from "./Connection";
+import { Serializer } from "./Serializer";
 
-export { Workspace };
-export { Block };
+export {
+    Workspace,
+    Block,
+    Events,
+    Connection,
+    Serializer
+};
 
 export class BlockBuilder extends Workspace {
+    public events: Events;
+
     constructor(container: HTMLElement) {
         super(container);
+
+        this.events = new Events();
+    }
+
+    save(): string {
+        return Serializer.save(this);
+    }
+
+    load(json: string): void {
+        Serializer.load(this, json);
     }
 }
 
-// Global browser API
 declare global {
     interface Window {
         BlockBuilder: typeof BlockBuilder;
