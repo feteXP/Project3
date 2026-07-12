dbe218492cc5c1017c52709a784a6c53d489fb6f
xTymezzAlt <fete3712@gmail.com>
2026-07-12 18:41:46 +0200

    Update Workspace.ts

--- src/Workspace.ts
+++ src/Workspace.ts
@@ -1,3 +1,22 @@
-class Workspace {
-  blocks = []
+import { Block } from "./Block";
+
+export class Workspace {
+    public svg: SVGSVGElement;
+    public blocks: Block[] = [];
+
+    constructor(container: HTMLElement) {
+        this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+        this.svg.setAttribute("width", "100%");
+        this.svg.setAttribute("height", "100%");
+        this.svg.style.background = "#202124";
+
+        container.appendChild(this.svg);
+    }
+
+    createBlock(text: string, x = 0, y = 0): Block {
+        const block = new Block(this.svg, text, x, y);
+        this.blocks.push(block);
+        return block;
+    }
 }
