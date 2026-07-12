d6f327f3933c2d19e11cd8f3b7aedc3c5522e7b8
xTymezzAlt <fete3712@gmail.com>
2026-07-12 19:41:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import { Block } from "./Block";
 import { Events } from "./Events";
 import { Connection } from "./Connection";
 import { Serializer } from "./Serializer";
+import { Drag } from "./Drag";
+import { Backup } from "./Backup";
+
 
 export {
     Workspace,
     Block,
     Events,
     Connection,
-    Serializer
+    Serializer,
+    Drag,
+    Backup
 };
+
 
 export class BlockBuilder extends Workspace {
     public events: Events;
+    public backups: Backup | null = null;
 
     constructor(container: HTMLElement) {
         super(container);
@@ -21,19 +28,52 @@ export class BlockBuilder extends Workspace {
         this.events = new Events();
     }
 
+
+    enableBackups(
+        interval = 5000
+    ): Backup {
+        this.backups = new Backup(
+            this,
+            "blockbuilder_backup",
+            interval
+        );
+
+        return this.backups;
+    }
+
+
     save(): string {
         return Serializer.save(this);
     }
 
-    load(json: string): void {
-        Serializer.load(this, json);
+
+    load(
+        json: string
+    ): void {
+        Serializer.load(
+            this,
+            json
+        );
+    }
+
+
+    restoreBackup(): boolean {
+        if (!this.backups) {
+            return false;
+        }
+
+        return this.backups.restore();
     }
 }
+
 
 declare global {
     interface Window {
         BlockBuilder: typeof BlockBuilder;
     }
 }
+
+
+window.BlockBuilder = BlockBuilder;
 
 window.BlockBuilder = BlockBuilder;
