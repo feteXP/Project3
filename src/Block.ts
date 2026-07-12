0428e0ee300e95361e5af15e5260a6d161c55464
xTymezzAlt <fete3712@gmail.com>
2026-07-12 18:40:51 +0200

    Update Block.ts

--- src/Block.ts
+++ src/Block.ts
@@ -1,12 +1,32 @@
-// Block.ts
 export class Block {
-    element: SVGGElement;
+    public group: SVGGElement;
+    public body: SVGRectElement;
+    public label: SVGTextElement;
 
-    constructor(svg: SVGSVGElement) {
-        this.element = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "g"
-        );
-        svg.appendChild(this.element);
+    constructor(
+        svg: SVGSVGElement,
+        text: string,
+        x: number = 0,
+        y: number = 0
+    ) {
+        this.group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        this.group.setAttribute("transform", `translate(${x}, ${y})`);
+
+        this.body = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        this.body.setAttribute("width", "120");
+        this.body.setAttribute("height", "40");
+        this.body.setAttribute("rx", "8");
+        this.body.setAttribute("fill", "#4CAF50");
+
+        this.label = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        this.label.setAttribute("x", "60");
+        this.label.setAttribute("y", "25");
+        this.label.setAttribute("text-anchor", "middle");
+        this.label.setAttribute("fill", "white");
+        this.label.textContent = text;
+
+        this.group.appendChild(this.body);
+        this.group.appendChild(this.label);
+        svg.appendChild(this.group);
     }
 }
